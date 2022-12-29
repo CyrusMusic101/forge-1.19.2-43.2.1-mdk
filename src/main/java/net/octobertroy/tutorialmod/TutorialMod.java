a1ff2cf3a1bffd4570530035331ebf94b9919679
CyrusMusic101 <31782334+CyrusMusic101@users.noreply.github.com>
2022-12-28 23:49:04 -0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/net/octobertroy/tutorialmod/TutorialMod.java
+++ src/main/java/net/octobertroy/tutorialmod/TutorialMod.java
@@ -12,6 +12,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.octobertroy.tutorialmod.block.ModBlocks;
 import net.octobertroy.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -24,6 +25,7 @@ public class TutorialMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
     }
